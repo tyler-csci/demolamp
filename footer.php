13a102649f000fe88645898b5410f6078d1bf1f3
Tyler Ewald <t.ewald95@gmail.com>
2019-11-03 19:21:57 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php 
+  echo <<<_END
+    </div>
+        <footer>
+            <h3>About the Template</h3>
+            <p>$clubstr is a template for MSU CSCI 491 students to create their own LAMP stack web application. Once up and running, this footer content and other code withing the project is intented to be replaced with custom modifications.</p>
+            <p>This assignment is derived from Robin Nixon's book, "Learning PHP, JavaScript, and MySQL" 5th Edition, (espececially Chapters 2, 8, and 27) available for online checkout through the MSU Library; original code samples (particularly the folder called <em>robinsnest</em> ) are available online at the book's <a href="http://lpmj.net/5thedition/">companion website</a>.</p>
+        </footer>
+        </div>
+    </body>
+</html>
+_END;
+?>
